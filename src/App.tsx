7ab595c8be3492ffe6ff0a9b018b85311326681b
Andrew <dracon.24041230@mail.ru>
2024-12-05 13:21:54 +0300

    added sessionStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './App.css';
 import styled from "styled-components";
 import {CounterSection} from "./components/counterSection/CounterSection";
@@ -7,25 +7,50 @@ import {Button} from "./components/button/Button";
 import {ButtonsWrapper} from "./components/ButtonsWrapper";
 
 
+type VariantsType = null | 'v1' | 'v2';
 
 function App() {
 
-    const [choice,setChoice] = useState<null|'v1'>(null);
+    const [choice, setChoice] = useState<VariantsType>(null);
     const [isOpenCounter, setIsOpenCounter] = useState(false);
 
-const onClickHandler = useCallback(()=>{
-    setIsOpenCounter(true);
-},[]);
+    const onClickHandler = useCallback(() => {
+        setIsOpenCounter(true);
+    }, []);
+
+    useEffect(() => {
+
+        let  result = sessionStorage.getItem('variant');
+
+        /*as VariantsType*/
+
+        if(result!=='null' && result!==null){
+           setChoice(result as VariantsType);
+           onClickHandler();
+       }
+
+    }, []);
+
+    const onClickPickVariantNumber1Handler = () => {
+        setChoice('v1');
+        sessionStorage.setItem('variant', 'v1');
+        onClickHandler();
+    };
+    const onClickPickVariantNumber2Handler = () => {
+        setChoice('v2');
+        sessionStorage.setItem('variant', 'v2');
+        onClickHandler();
+    };
 
     return (
         <Wrapper>
             <Container>
-                {!isOpenCounter &&<> <Title>Choose a version of counter</Title>
-                <ButtonsWrapper>
-                <Button onClick={()=>{setChoice(null);onClickHandler()}}>#1</Button>
-                <Button onClick={()=>{setChoice('v1');onClickHandler()}}>#2</Button>
-                </ButtonsWrapper></>}
-                {isOpenCounter && <CounterSection mode2={choice==='v1'} />}
+                {!isOpenCounter && <> <Title>Choose a version of counter</Title>
+                    <ButtonsWrapper>
+                        <Button onClick={onClickPickVariantNumber1Handler}>#1</Button>
+                        <Button onClick={onClickPickVariantNumber2Handler}>#2</Button>
+                    </ButtonsWrapper></>}
+                {isOpenCounter && <CounterSection mode2={choice === 'v1'}/>}
 
             </Container>
         </Wrapper>
@@ -43,12 +68,13 @@ const Wrapper = styled.div`
         max-width: 400px;
         margin: 0 auto;
     }
+
     ${Container} > ${ButtonsWrapper} > button {
         padding: 0 40px;
         line-height: 2;
     }
-        
-    }
+
+}
 `;
 
 
