451692239d39cb622860e5f2d208117620b605a2
Andrew <dracon.24041230@mail.ru>
2025-02-05 13:04:03 +0300

    refactor Redux v.0.3

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import styled from "styled-components";
 import {CounterSection} from "./components/counterSection/CounterSection";
 import {Container} from "./components/Container";
 import {ButtonsWrapper} from "./components/ButtonsWrapper";
-import {ButtonMui} from "./components/button/ButtonMUI";
-import {AppBar, Box, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography} from "@mui/material";
+import {
+    AppBar,
+    Box,
+    createTheme,
+    CssBaseline,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    ThemeProvider,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import {DarkLightSwitcher} from "./components/switches/darkLight/DarkLightSwitch";
 import {Wrapper} from "./components/Wrapper";
 import {Version} from "./common/enums";
@@ -19,76 +31,97 @@ function App() {
     /*const [choice, setChoice] = useState<Version>(Version.notChoice);*/
     const [isOpenCounter, setIsOpenCounter] = useState(false);
     /*const [themeMode, setThemeMode] = useState<PaletteMode>('light');*/
-        const version = useAppSelector(getVersionCounter);
-        const themeMode = useAppSelector(getThemeMode);
-        const dispatch = useAppDispatch();
+    const version = useAppSelector(getVersionCounter);
+    const themeMode = useAppSelector(getThemeMode);
+    const dispatch = useAppDispatch();
 
 
+    const changeVersion = useCallback((ver: Version) => {
 
 
+        if (ver === Version.notChoice) {
+            setIsOpenCounter(false);
+        }
+        else{
+            setIsOpenCounter(true);
 
-    const changeFirstVersion = useCallback(() => {
+        }
+        dispatch(changeVersionCounter(ver));
 
-        dispatch(changeVersionCounter(Version.v1));
-        setIsOpenCounter(true);
-    },[dispatch]);
-
-    const changeSecondVersion = useCallback(() => {
-        dispatch(changeVersionCounter(Version.v2));
-        setIsOpenCounter(true);
-    },[dispatch]);
-
+    }, [dispatch]);
 
 
     const changeTheme = useCallback(() => {
-        dispatch(changeThemeMode(themeMode==='light' ? 'dark' :'light'));
+        dispatch(changeThemeMode(themeMode === 'light' ? 'dark' : 'light'));
 
 
-    },[themeMode,dispatch]);
+    }, [themeMode, dispatch]);
 
-     const theme = createTheme({
+    const theme = createTheme({
 
         palette: {
-            mode:themeMode,
+            mode: themeMode,
             primary: {
                 main: '#05f7ff',
-               /* dark: 'red',*/
-                light: 'rgb(59,89,97)',
+                light: '#023e49',
+
+
             },
             secondary: {
-                main: '#640db3',
+                main: '#15205c',
             },
+
+
         },
     });
     return <AppStyled>
         <ThemeProvider theme={theme}>
-            <CssBaseline />
+            <CssBaseline/>
             <AppBar component="nav">
-                <Toolbar>
+                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
 
                     <Typography
                         variant="h6"
                         component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                        sx={{flexGrow: 0, display: {xs: 'none', sm: 'block'}}}
                     >
                         Counter
                     </Typography>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        <DarkLightSwitcher onClick={changeTheme} />
+                    <FormControl color={theme.palette.mode === 'dark' ? 'primary' : 'secondary'} size={'small'}
+                                 sx={{width: 'auto'}}>
+                        {!isOpenCounter && <InputLabel sx={{position: 'absolute', top: -4, left: -4}}
+                                                       id="version">Version</InputLabel>}
+                        <Select
+                            labelId="version"
+
+                            value={version}
+
+                            onChange={(e: SelectChangeEvent<Version>) => {
+                                changeVersion(e.target.value as Version)
+                            }}
+                        >
+                            <MenuItem value={Version.notChoice}>Default</MenuItem>
+                            <MenuItem value={Version.v1}>v1</MenuItem>
+                            <MenuItem value={Version.v2}>v2</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                    <Box sx={{display: {xs: 'none', sm: 'block'}}}>
+                        <DarkLightSwitcher onClick={changeTheme}/>
                     </Box>
                 </Toolbar>
             </AppBar>
-        <Wrapper>
-            <Container>
-                {!isOpenCounter && <> <Title>Choose a version of counter</Title>
-                    <ButtonsWrapper>
-                        <ButtonMui onClick={changeFirstVersion}>#1</ButtonMui>
-                        <ButtonMui onClick={changeSecondVersion}>#2</ButtonMui>
-                    </ButtonsWrapper></>}
-                {isOpenCounter && <CounterSection mode2={version === Version.v1}/>}
+            <Wrapper>
+                <Container>
+                    {!isOpenCounter && <>
+                        <Title>Choose a version of counter</Title>
 
-            </Container>
-        </Wrapper>
+                    </>
+                    }
+                    {isOpenCounter && <CounterSection mode2={version === Version.v1}/>}
+
+                </Container>
+            </Wrapper>
         </ThemeProvider>
     </AppStyled>;
 }
@@ -101,7 +134,7 @@ const AppStyled = styled.div`
         max-width: 400px;
         margin: 0 auto;
     }
-    
+
 }
 `;
 
@@ -114,6 +147,7 @@ const Title = styled.h1`
 `;
 
 
-
-
-
+/*<ButtonsWrapper>
+        <ButtonMui onClick={changeFirstVersion}>#1</ButtonMui>
+        <ButtonMui onClick={changeSecondVersion}>#2</ButtonMui>
+    </ButtonsWrapper></>*/
